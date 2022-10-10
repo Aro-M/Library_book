2d3d10a3a7252fcb765cbebfcdb5b9585c63aa96
Aro MIlitosyan <ar.militosyan.97@mail.ru>
2022-10-10 13:05:09 +0400

    Update Library.py

--- Library.py
+++ Library.py
@@ -4,14 +4,13 @@ import  sqlite3
 class  Library:
     def  __init__(self):
         self.lib_book = {}
-        
+
     def  add_book(self,book_name,book_rating):
         if book_name  is not  self.lib_book.keys():
            self.lib_book[book_name] = book_rating
         else:
             print(f"{book_name} Book already  in the  Library with Book ratings {self.lib_book.get(book_name)}")
 
-            
     def  change_book_name(self,book_name,new_book_name,book_rating):
         if  book_name not in self.lib_book.keys():
             print(f"\n {book_name} Book does not exist  in the  Library ")
@@ -27,7 +26,6 @@ class  Library:
             self.lib_book[book_name] = new_rating
             print(f"\n{book_name} Book   rating  updated to {new_rating}"  )
 
-            
     def  get_all_book(self):
           for  i in self.lib_book:
             print(f"\n {i} , {self.lib_book[i]}")
@@ -64,7 +62,24 @@ class  Library:
             json_lib_read = json.load(inLib_read)
         for i in json_lib_read:
             print(f"{i} ,{json_lib_read[i]} \n")
-  
+
+    def  send_database_lib(self):
+        conn = sqlite3.connect('library.db')
+        cur = conn.cursor()
+        cur.execute('DROP TABLE IF EXISTS Library')
+        cur.execute('CREATE TABLE Library (Book_name TEXT, Book_rating INTEGER)')
+        for i in self.lib_book:
+            cur.execute('INSERT INTO Library (Book_name, Book_rating) VALUES (?, ?)',
+                    (i, self.lib_book[i]))
+        conn.commit()
+        print('Tracks:')
+        cur.execute('SELECT Book_name,Book_rating FROM Library')
+        for row in cur:
+            print(row)
+
+        cur.close()
+
+
 
 
 
@@ -104,6 +119,6 @@ library.change_book_name(book_name="Mark Lutz" ,new_book_name="fl clear",book_ra
 
 library.set_data_json_file()
 library.get_data_json_file()
-# library.open_data_sqlite()
+library.send_database_lib()
 
 
